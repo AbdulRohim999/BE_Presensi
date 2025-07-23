c51340644ae4a5bf12e02ad5bcab475b3679baa9
abdulrohim100699 <abdulrohim100699@gmail.com>
2025-07-23 15:35:05 +0700

    Update

--- src/main/java/com/example/e_presensi/config/OpenApiConfig.java
+++ src/main/java/com/example/e_presensi/config/OpenApiConfig.java
@@ -1,12 +1,16 @@
 package com.example.e_presensi.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -30,5 +34,13 @@ public class OpenApiConfig {
                         .title("E-Presensi API")
                         .description("API Documentation untuk E-Presensi")
                         .version("1.0"));
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://epresensi-sttp.up.railway.app");
+        server.setDescription("Production Server (HTTPS)");
+        return new OpenAPI().servers(List.of(server));
     }
 }
